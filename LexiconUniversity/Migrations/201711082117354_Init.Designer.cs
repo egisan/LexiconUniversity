d92c0a3e2ba8d9b37da85382955f88e6d1133008
Egidio Ricchiuti <egisan3@yahoo.com>
2017-11-08 22:24:45 +0100

    re-create DB

--- LexiconUniversity/Migrations/201711082117354_Init.Designer.cs
+++ LexiconUniversity/Migrations/201711082117354_Init.Designer.cs
@@ -6,14 +6,14 @@ namespace LexiconUniversity.Migrations
     using System.Data.Entity.Migrations.Infrastructure;
     using System.Resources;
     
-    [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class init : IMigrationMetadata
+    [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
+    public sealed partial class Init : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(init));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(Init));
         
         string IMigrationMetadata.Id
         {
-            get { return "201710310850498_init"; }
+            get { return "201711082117354_Init"; }
         }
         
         string IMigrationMetadata.Source
